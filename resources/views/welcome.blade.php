ab5b7058d253637b9c3c02a05000b6c7d64aa936
Azamat <azamailnet@mail.ru>
2023-02-19 12:38:23 +0600

    updated to Laravel 10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,5 +42,8 @@
             @endif
             </div>
         </div>
+        <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+            Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+        </div>
     </body>
 </html>
